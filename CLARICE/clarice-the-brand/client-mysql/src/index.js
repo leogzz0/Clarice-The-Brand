f022b88af2792708f65b5b0c8e1c7691e8246b9f
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-10-31 09:29:31 -0600

    fix: favicon change state with os theme

--- CLARICE/clarice-the-brand/client-mysql/src/index.js
+++ CLARICE/clarice-the-brand/client-mysql/src/index.js
@@ -5,6 +5,19 @@ import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+function setFavicon(theme) {
+  const favicon = document.querySelector('link[rel=\'icon\']');
+  if (theme === 'dark') {
+    favicon.href = './img/clarice_white.png';
+  } else {
+    favicon.href = './img/clarice_black.png';
+  }
+}
+
+const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+setFavicon(theme);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

9add7be673aaf2a0b1d6959a26f5c442b89a5ddd
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-11-03 14:29:50 -0600

    fix: navbar feature in About.jsx

--- CLARICE/clarice-the-brand/client-mysql/src/pages/About/About.jsx
+++ CLARICE/clarice-the-brand/client-mysql/src/pages/About/About.jsx
@@ -8,35 +8,38 @@ const About = () => {
   ];
 
   return (
-    <div className="about-container">
-      <div className="about-text">
-        <h1>ABOUT US.</h1>
-        <br />
-        <p>
-          Clarice The Brand stands for all those fashionistas out there who are willing to step up their looks.
+    <>
+      <div className='navbar-spacer'></div>
+      <div className="about-container">
+        <div className="about-text">
+          <h1>ABOUT US.</h1>
+          <br />
+          <p>
+            Clarice The Brand stands for all those fashionistas out there who are willing to step up their looks.
 
-          We help you elevate your style and become the most confident version of yourself through your shoes so that you can be your true, most fashion self. 
+            We help you elevate your style and become the most confident version of yourself through your shoes so that you can be your true, most fashion self. 
 
-          Marylin Monroe once said, “Give a woman the right shoes and she can conquer the world” and we couldn’t agree more! 
-        </p>
-        <br />
-        <p>
-          We believe in your power to accomplish anything your heart desires and we are here to help you step out in style, step up in power.
+            Marylin Monroe once said, “Give a woman the right shoes and she can conquer the world” and we couldn’t agree more! 
+          </p>
+          <br />
+          <p>
+            We believe in your power to accomplish anything your heart desires and we are here to help you step out in style, step up in power.
 
-          In Clarice The Brand, we create It shoes for It girls
-          We limit our pieces to offer you exclusiveness so that you can continue to elevate your authentic self.
+            In Clarice The Brand, we create It shoes for It girls
+            We limit our pieces to offer you exclusiveness so that you can continue to elevate your authentic self.
 
 
-          Welcome to the Clarice The Brand community, we’re glad you’re here
-        </p>
-      </div>
-      <div className="about-image">
-        <img src={images[0]} alt="About Us" />
-        <div className="quote">
-          <p>Give a woman the right shoes and she can conquer the world.</p>
+            Welcome to the Clarice The Brand community, we’re glad you’re here
+          </p>
+        </div>
+        <div className="about-image">
+          <img src={images[0]} alt="About Us" />
+          <div className="quote">
+            <p>Give a woman the right shoes and she can conquer the world.</p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

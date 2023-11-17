d3456d4292f8bada766548b4c5cf1cdf33515635
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-11-17 12:52:11 -0600

    update countdown layout

--- CLARICE/countdown/main.js
+++ CLARICE/countdown/main.js
@@ -37,3 +37,12 @@ function tick () {
     document.querySelector('.countdown').innerText = time;
   }
 }
+
+function changeBackground() {
+  var images = ["64-SAM_0397.jpg", "88-SAM_0359.jpg", "138-SAM_0271.jpg"];
+  var randomizer = Math.floor(Math.random() * images.length);
+  var selectedImage = "assets/" + images[randomizer];
+  document.querySelector('header').style.backgroundImage = "url('" + selectedImage + "')";
+}
+
+document.addEventListener("DOMContentLoaded", changeBackground);

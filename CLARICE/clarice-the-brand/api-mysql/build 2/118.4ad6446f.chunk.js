626572032c4136d616ce26ea1e168f57515e37c6
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-12-13 23:53:30 -0600

    Added LICENSE and README

--- CLARICE/clarice-the-brand/api-mysql/build 2/118.4ad6446f.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapi_mysql=self.webpackChunkapi_mysql||[]).push([[118],{10118:(l,s,u)=>{u.r(s),u.d(s,{default:()=>a});const a={}}}]);
--- CLARICE/clarice-the-brand/api-mysql/build 2/9492.bbc2ee05.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapi_mysql=self.webpackChunkapi_mysql||[]).push([[9492],{59492:(a,s,l)=>{l.r(s),l.d(s,{default:()=>u});const u={}}}]);

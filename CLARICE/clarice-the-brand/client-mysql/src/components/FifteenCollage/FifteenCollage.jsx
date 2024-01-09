164013bc7533141852a70b2d1682d6d84eba34fd
Leonardo Gonzalez <leogzz0@hotmail.com>
2024-01-08 18:18:04 -0600

    New image format in editorial data

--- CLARICE/clarice-the-brand/client-mysql/src/components/FifteenCollage/FifteenCollage.jsx
+++ CLARICE/clarice-the-brand/client-mysql/src/components/FifteenCollage/FifteenCollage.jsx
@@ -12,36 +12,36 @@ const FifteenCollage = ({ photos }) => {
       </div>
       <img src={photos[3]} alt="photo 3" className="full-width" />
       <div className="half-width">
-        <img src={photos[4]} alt="photo 5" />
-        <img src={photos[5]} alt="photo 6" />
+        <img src={photos[4]} alt="photo 4" />
+        <img src={photos[5]} alt="photo 5" />
       </div>
       <div className="half-width">
-        <img src={photos[6]} alt="photo 7" />
-        <img src={photos[7]} alt="photo 8" />
+        <img src={photos[6]} alt="photo 6" />
+        <img src={photos[7]} alt="photo 7" />
       </div>
-      <img src={photos[8]} alt="photo 9" className="full-width" />
+      <img src={photos[8]} alt="photo 8" className="full-width" />
       <div className="half-width">
-        <img src={photos[9]} alt="photo 10" />
-        <img src={photos[10]} alt="photo 11" />
+        <img src={photos[9]} alt="photo 9" />
+        <img src={photos[10]} alt="photo 10" />
       </div>
-      <img src={photos[11]} alt="photo 12" className="full-width" />
+      <img src={photos[11]} alt="photo 11" className="full-width" />
       <div className="half-width">
-        <img src={photos[12]} alt="photo 13" />
-        <img src={photos[13]} alt="photo 14" />
+        <img src={photos[12]} alt="photo 12" />
+        <img src={photos[13]} alt="photo 13" />
       </div>
-      <img src={photos[14]} alt="photo 15" className="full-width" />
+      <img src={photos[14]} alt="photo 14" className="full-width" />
       <div className="half-width">
-        <img src={photos[15]} alt="photo 16" />
-        <img src={photos[16]} alt="photo 17" />
+        <img src={photos[15]} alt="photo 15" />
+        <img src={photos[16]} alt="photo 16" />
       </div>
       <div className="half-width">
-        <img src={photos[17]} alt="photo 18" />
-        <img src={photos[18]} alt="photo 19" />
+        <img src={photos[17]} alt="photo 17" />
+        <img src={photos[18]} alt="photo 18" />
       </div>
-      <img src={photos[19]} alt="photo 20" className="full-width" />
+      <img src={photos[19]} alt="photo 19" className="full-width" />
       <div className="half-width">
-        <img src={photos[20]} alt="photo 21" />
-        <img src={photos[21]} alt="photo 22" />
+        <img src={photos[20]} alt="photo 20" />
+        <img src={photos[21]} alt="photo 21" />
       </div>
     </div>
   );

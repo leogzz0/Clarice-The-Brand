1589ad0868bea9a4af49b3e05a9ac2a89b090748
Leonardo Gonzalez <leogzz0@hotmail.com>
2024-01-24 20:08:23 -0600

    Added Editorial page test file

---
+++ CLARICE/clarice-the-brand/client-mysql/src/pages/Editorial/Editorial.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Editorial from './Editorial';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const EditorialData = [
+  {
+    imageUrl: '/img/Editorial Clarice/DSC_1426.JPG',
+    season: 'Clarice The Brand',
+    launchTitle: 'La Beauté Collection',
+    link: '/',
+  },
+];
+
+jest.mock('../../contexts/NavbarContext', () => ({
+  useNavbarVisibility: () => ({
+    setIsVisible: jest.fn(),
+  }),
+}));
+
+describe('Editorial Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <Router>
+        <Editorial />
+      </Router>
+    );
+
+    EditorialData.forEach((item) => {
+      expect(screen.getByText(item.season)).toBeInTheDocument();
+      expect(screen.getByAltText(item.launchTitle)).toBeInTheDocument();
+    });
+  });
+});

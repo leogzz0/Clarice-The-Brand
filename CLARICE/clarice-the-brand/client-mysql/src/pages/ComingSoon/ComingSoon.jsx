dff64531b8f72a02eba4e49cc63685a01529c759
Leonardo Gonzalez <leogzz0@hotmail.com>
2024-02-01 15:42:43 -0600

    Add "Keep Shopping" button to ComingSoon page

--- CLARICE/clarice-the-brand/client-mysql/src/pages/ComingSoon/ComingSoon.jsx
+++ CLARICE/clarice-the-brand/client-mysql/src/pages/ComingSoon/ComingSoon.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './ComingSoon.scss';
 
 const ComingSoon = () => {
+  const navigate = useNavigate();
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
     <div className="coming-soon-container">
       <h1>Coming Soon</h1>
       <p>We are currently working on enhancing your user experience. Our team is dedicated to bringing you the best features. Stay tuned!</p>
+      <button className="home-button" onClick={handleGoHome}>Keep Shopping</button>
     </div>
   );
 };

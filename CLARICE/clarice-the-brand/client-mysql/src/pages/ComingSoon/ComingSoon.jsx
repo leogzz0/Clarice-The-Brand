42afaadbbc9221af9949a0d1d40ef21eb59179d2
Leonardo Gonzalez <leogzz0@hotmail.com>
2024-02-01 15:27:55 -0600

    New coming soon page route and layout

---
+++ CLARICE/clarice-the-brand/client-mysql/src/pages/ComingSoon/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ComingSoon.scss';
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <h1>Coming Soon</h1>
+      <p>We are currently working on enhancing your user experience. Our team is dedicated to bringing you the best features. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default ComingSoon;

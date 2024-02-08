8cc06f86bb1b36987e1228221057b521391e9ede
Leonardo Gonzalez <leogzz0@hotmail.com>
2024-02-08 12:37:16 -0600

    Remove unused chunk files

--- CLARICE/clarice-the-brand/api-mysql/build 2/1345.7f3d0bd5.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapi_mysql=self.webpackChunkapi_mysql||[]).push([[1345],{21345:(a,s,u)=>{u.r(s),u.d(s,{default:()=>l});const l={}}}]);
--- CLARICE/clarice-the-brand/api-mysql/build 2/7086.94a1eafa.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapi_mysql=self.webpackChunkapi_mysql||[]).push([[7086],{57086:(a,s,u)=>{u.r(s),u.d(s,{default:()=>l});const l={}}}]);
